5c5e9e02dba2def77cf22fcdcf4f5d7f99b0e787
shriram ms <dev.shriram.ms@gmail.com>
2023-01-07 20:37:08 +0530

    1833. Maximul Ice cream bars

--- src/Arrays/Leetcode_1833_Maximum_icecream_bars.java
+++ src/Arrays/Leetcode_1833_Maximum_icecream_bars.java
@@ -34,7 +34,7 @@ class MaximumIceCreamBars {
 
     public static void maxIceCream(int[] costs, int coin) {
         int count =0;
-        //
+        // Sorting the array
         Arrays.sort(costs);
         for(int i: costs){
             if(coin -i >=0 ){
